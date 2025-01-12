2d5ab98f5a3ee262d9239e48bbe8ebaf97e6b6cd
Sayantan Khan <sayantan@Sayantans-Mac-mini.local>
2025-01-12 00:54:35 -0500

    Made initial working version

---
+++ darwin.nix
@@ -0,0 +1,27 @@
+# darwin.nix
+# MacOS specific configs live here
+{  pkgs, ... }: {
+    # List packages installed in system profile. To search by name, run:
+    # $ nix-env -qaP | grep wget
+    environment.systemPackages =
+    [ pkgs.vim
+    ];
+
+    # Necessary for using flakes on this system.
+    nix.settings.experimental-features = "nix-command flakes";
+
+    # Enable alternative shell support in nix-darwin.
+    # programs.fish.enable = true;
+
+    # Used for backwards compatibility, please read the changelog before changing.
+    # $ darwin-rebuild changelog
+    system.stateVersion = 5;
+
+    # The platform the configuration will be used on.
+    nixpkgs.hostPlatform = "aarch64-darwin";
+
+    users.users.sayantan = {
+        name = "sayantan";
+        home = "/Users/sayantan";
+    };
+}
