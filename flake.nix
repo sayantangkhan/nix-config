e068fbd149fa39528277439b7844cde7f0464d20
Sayantan Khan <sayantangkhan@gmail.com>
2025-01-12 11:08:09 -0500

    Unified flake files

--- flake.nix
+++ flake.nix
@@ -1,1 +1,36 @@
-flake-macbook-pro.nix
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }: {
+    darwinConfigurations."Sayantans-Mac-mini" = nix-darwin.lib.darwinSystem {
+      modules = [
+        ./darwin.nix
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.sayantan = import ./home-darwin.nix;
+        }
+      ];
+    };
+    darwinConfigurations."Sayantans-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      modules = [
+        ./darwin.nix
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.sayantan = import ./home-darwin.nix;
+        }
+      ];
+    };
+  };
+}
