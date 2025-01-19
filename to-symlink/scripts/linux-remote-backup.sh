3b09625cadc64a635e5637de739cf47b8507838c
Sayantan Khan <sayantangkhan@gmail.com>
2025-01-19 13:41:02 -0500

    Fixed typo in script

--- to-symlink/scripts/linux-remote-backup.sh
+++ to-symlink/scripts/linux-remote-backup.sh
@@ -1,3 +1,3 @@
 #!/run/current-system/sw/bin/zsh
 
-sudo --preserver-env=PATH env btrbk -c /home/sayantan/.config/btrbk/btrbk-remote.conf -v run
+sudo --preserve-env=PATH env btrbk -c /home/sayantan/.config/btrbk/btrbk-remote.conf -v run
